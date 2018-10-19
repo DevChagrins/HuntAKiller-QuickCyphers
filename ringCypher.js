c24c50ec8917ac0dceb53715fcf1092a312c87c5
malcolmjgruber@gmail.com <malcolmjgruber@gmail.com>
2018-10-18 21:39:48 -0500

    Takes in parameters

--- ringCypher.js
+++ ringCypher.js
@@ -1,15 +1,42 @@
-var values = "S13, G7, G5, S9, G8, S26, G14, S20, G9, S13, S7, S19, G5, S9, S22, G10, S6, S8, G20, G1, S15, G9, S7, G20, G12, S22, S15, G15";
+const minimist = require('minimist');
 
+// Sample string, splits on ',': var values = "S13, G7, G5, S9, G8, S26, G14, S20, G9, S13, S7, S19, G5, S9, S22, G10, S6, S8, G20, G1, S15, G9, S7, G20, G12, S22, S15, G15";
+var input = "";
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var arrayValues = values.split(",");
+// Parse the arguments
+var args = minimist(process.argv.slice(2), { alias: {i: "input", s: "split"} });
+
+// Make sure there is input
+if(args["input"]) {
+	input = args["input"];
+}
+
+// Exits early if there is no input
+if(input.length === 0)
+{
+	console.log("No input detected. Make sure you use -i <input> or --input=<input>");
+	return;
+}
+
+// Checks for a modified string to split on, defaults to a space
+var splitInput = []
+var splitParam = " ";
+
+if(args["split"]) {
+	splitParam = args["split"];
+}
+
+// Splits the input into an array
+var splitInput = input.split(splitParam);
+
+// Splits each piece into Letter and Number pairs (example: S13 becomes [S, 13])
 var splitPairs = [];
-arrayValues.forEach(value => {
+splitInput.forEach(value => {
 	splitPairs.push(value.replace(/\'/g, '').split(/(\d+)/).filter(Boolean));
 });
-	
-//console.log(output);
 
+// Loops through each pair and gets the appropriate character, adds it to the string and outputs it
 var stringReturn = "";
 splitPairs.forEach(pair => {
 	var key = pair[0].replace(/\s/g,'').toUpperCase();
@@ -19,7 +46,7 @@ splitPairs.forEach(pair => {
 		index = alphabet.length - value;
 	}
 	else if(key === "G") {
-		index = value;
+		index = value - 1;
 	}
 	else {
 		console.log("FUCK");
@@ -28,6 +55,7 @@ splitPairs.forEach(pair => {
 	stringReturn += alphabet[index];
 });
 
+// Does the above for only for S
 var silverString = "";
 splitPairs.forEach(pair => {
 	var key = pair[0].replace(/\s/g,'').toUpperCase();
@@ -45,6 +73,7 @@ splitPairs.forEach(pair => {
 
 });
 
+// Does the above for only for G
 var goldString = "";
 splitPairs.forEach(pair => {
 	var key = pair[0].replace(/\s/g,'').toUpperCase();
@@ -53,7 +82,7 @@ splitPairs.forEach(pair => {
 	if(key === "S") {
 	}
 	else if(key === "G") {
-		index = value
+		index = value - 1;
 		goldString += alphabet[index];
 	}
 	else {
